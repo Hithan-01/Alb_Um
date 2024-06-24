bceb5733edce64c242eece4dfdafc90b52ae03f6
Hithan-01 <1200455@alumno.um.edu.mx>
2024-06-24 00:24:08 -0600

    Proyecto Base

---
+++ src/main/java/com/Demo/Alb_Um/AlbUmApplication.java
@@ -0,0 +1,13 @@
+package com.Demo.Alb_Um;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbUmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Demo/Alb_Um/AlbUmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Demo.Alb_Um;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
